eeaae92109fe69a14bf570d6e53396d077d71bc4
Aman Shrivastava <amanshrivastava118@gmail.com>
2026-06-14 22:40:05 +0530

    Add dev-cluster-1 workload cluster

--- terraform/management-cluster/main.tf
+++ terraform/management-cluster/main.tf
@@ -168,7 +168,7 @@ resource "aws_eks_node_group" "mgmt" {
   node_group_name = "mgmt-nodes"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = aws_subnet.private[*].id
-  instance_types  = ["t3.small"]
+  instance_types  = ["m7i-flex.large"]
 
   scaling_config {
     desired_size = 1
